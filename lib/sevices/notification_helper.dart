757dc1ef037a72e760ea89a2429c47cfe870de7f
aishah-sha <n.aishahbahar@gmail.com>
2025-06-04 23:09:16 +0800

    5th_commit_try_addingDatabase_addNotification_eventPage

--- lib/sevices/notification_helper.dart
+++ lib/sevices/notification_helper.dart
@@ -2,7 +2,7 @@ import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:timezone/data/latest.dart' as tz;
 import 'package:timezone/timezone.dart' as tz;
 
-class NotificationHelper {
+class NotificationHelper {}
   static final FlutterLocalNotificationsPlugin _notificationsPlugin =
       FlutterLocalNotificationsPlugin();
 
